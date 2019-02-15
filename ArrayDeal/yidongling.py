980414c60ca2618a5c9d39e10b457c3f080f3a89
wb.zhanglu <wb.zhanglu@mesg.corp.netease.com>
2019-02-15 14:33:39 +0800

    痴树花

---
+++ ArrayDeal/yidongling.py
@@ -0,0 +1,18 @@
+#-*-coding:utf-8-*-
+
+class Solution:
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        n = nums.count(0)
+
+        for i in range(n):
+            nums.remove(0)
+        nums.extend([0]*n)
+        print(nums)
+if __name__ == '__main__':
+    nums = [0,1,0,3,12]
+
+    print(Solution().moveZeroes(nums))
